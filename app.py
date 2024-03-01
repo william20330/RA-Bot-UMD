327e9cab74185a647c814b0ce4f469035481c32d
William Phoone <william20330@gmail.com>
2024-03-01 12:35:23 -0500

    test2

--- app.py
+++ app.py
@@ -1,52 +1,33 @@
 import os
-from flask import Flask, request, jsonify
-import requests
+import sys
+from urllib.parse import urlencode
+from urllib.request import urlopen
+from flask import Flask, Request, request
 
 app = Flask(__name__)
 
-# Variable to track if the bot is active
-bot_active = False
+@app.route('/', methods=['POST'])
+def webhook():
+  data = request.get_json()
+  log('Recieved {}'.format(data))
 
-# Define a route to handle GroupMe callbacks
-@app.route('/callback', methods=['POST'])
-def callback():
-    global bot_active
-    data = request.get_json()
+  # We don't want to reply to ourselves!
+  if data['name'] != 'ra-bot-umdt':
+    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+    send_message(msg)
 
-    # Ensure the request is from GroupMe
-    if 'text' in data and 'group_id' in data:
-        message_text = data['text'].lower()
-        group_id = data['group_id']
+  return "ok", 200
 
-        if message_text == 'raBot':
-            bot_active = True
-            send_message(group_id, "RA Bot is now active. Please select an option from the menu.")
-        elif bot_active:
-            if message_text == '1':
-                send_message(group_id, "Number for 4Work (Facilities Issues, Hygiene Issues, etc): 301-314-9675")
-            elif message_text == '2':
-                send_message(group_id, "UMD Dining Hall Hours: Monday - Friday: 7:00 am - 8:00 pm")
-            elif message_text == '3':
-                bot_active = False
-                send_message(group_id, "RA Bot has been deactivated.")
-            else:
-                send_message(group_id, "Invalid option. Please select a valid option.")
-        
-    return jsonify({'status': 'success'})
+def send_message(msg):
+  url  = 'https://api.groupme.com/v3/bots/post'
 
-# Function to send a message to a GroupMe group
-def send_message(group_id, text):
-    bot_id = os.getenv('GROUPME_BOT_ID')
-    url = f'https://api.groupme.com/v3/bots/post'
-
-    payload = {
-        'bot_id': bot_id,
-        'text': text,
-        'attachments': []
-    }
-
-    response = requests.post(url, json=payload)
-    response.raise_for_status()
-
-if __name__ == '__main__':
-    app.run(debug=True)
+  data = {
+          'bot_id' : os.getenv('GROUPME_BOT_ID'),
+          'text'   : msg,
+         }
+  request = Request(url, urlencode(data).encode())
+  json = urlopen(request).read().decode()
+  
+def log(msg):
+  print(str(msg))
+  sys.stdout.flush()
