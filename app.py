e3ed7bc79841a3d221f37547cc54793230f79c5d
William Phoone <william20330@gmail.com>
2024-03-01 14:50:27 -0500

    bleh

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ import requests
 from flask import Flask, request
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
-scheduler = BackgroundScheduler()
+import time
 
 app = Flask(__name__)
 scheduler = BackgroundScheduler()
 bot_active = False  # Variable to keep track of whether the bot is active or not
+# Define a variable to track the last time a message was sent
+last_message_sent_time = 0
+# Define a cooldown period in seconds (e.g., 60 seconds)
+cooldown_period = 60
 
 def check_and_send_holiday_message():
     # Replace 'us' with the correct country code
@@ -33,8 +37,8 @@ def start_scheduler():
 
 @app.route('/', methods=['POST'])
 def webhook():
-    global bot_active  # Accessing the global bot_active variable
-
+    global bot_active, last_message_sent_time
+    
     data = request.get_json()
     log('Received {}'.format(data))
     
@@ -44,39 +48,24 @@ def webhook():
         send_message('- Type /menu for more options and resources\n - Type /exit at anytime to leave the bot')
     # Check if the bot is active and respond to user commands
     elif bot_active:
-        if '/menu' in data['text'].lower():
-            send_message('Here are the options (press number associated with choice): \
-                        \n 1 - Phone Number for 4Work (issues regarding facilites, cleanliness, etc) \
-                        \n 2 - Phone Number for the Cumberland Front Desk (contact RA on Duty, lockouts, etc) \
-                        \n 3 - Hours of Operation For Dining Halls \
-                        \n 4 - Important Links (ResLife, 4Work, etc)\
-                        \n 5 - Important Dates (closues, breaks, finals, etc)\
-                        \n 6 - UMD Sports Schedule/Scores')
-        elif '1' in data['text'] and bot_active:
-            send_message('Phone Number for 4Work: 301-314-9675')
-        elif '2' in data['text'] and bot_active:
-            send_message('Phone Number for the Cumberland Front Desk: 301-314-2862')
-        elif '3' in data['text'] and bot_active:
-            send_message('Dining Hall Hours: \
-                        \n Yahentamitsi: Monday - Friday: 7:00am - 9:00pm | Saturday - Sunday: 10:00am - 9:00pm\
-                        \n 251 North: Monday - Thursday: 8:00am - 10:00pm | Friday - Sunday: 8:00am - 7:00pm\
-                        \n South Campus: Monday - Friday: 7:00am - 9:00pm | Saturday - Sunday: 10:00am - 9:00pm')
-        elif '4' in data['text'] and bot_active:
-            send_message('Important Links: \
-                        \n ResLife: https://reslife.umd.edu/ \
-                        \n 4Work: https://4work.umd.edu/ \
-                        \n UMD: https://www.umd.edu/\
-                        \n Dining Services: https://dining.umd.edu/\
-                        \n StarRez: https://www.starrez.umd.edu/')
-        elif '5' in data['text'] and bot_active:
-            send_message('Important Dates: \
-                        \n Spring Break: March 17-24\
-                        \n Last Day of Classes: May 9\
-                        \n Reading Day: May 10\
-                        \n Finals: May 11-17')
-        elif '/exit' in data['text'].lower() and bot_active:
-            send_message('Goodbye!')
-            bot_active = False  # Deactivate the bot
+        # Check if the cooldown period has elapsed since the last message was sent
+        if time.time() - last_message_sent_time >= cooldown_period:
+            if '/menu' in data['text'].lower():
+                send_message('Here are the options (press number associated with choice): \
+                            \n 1 - Phone Number for 4Work (issues regarding facilites, cleanliness, etc) \
+                            \n 2 - Phone Number for the Cumberland Front Desk (contact RA on Duty, lockouts, etc) \
+                            \n 3 - Hours of Operation For Dining Halls \
+                            \n 4 - Important Links (ResLife, 4Work, etc)\
+                            \n 5 - Important Dates (closues, breaks, finals, etc)\
+                            \n 6 - UMD Sports Schedule/Scores')
+            elif '1' in data['text'] and bot_active:
+                send_message('Phone Number for 4Work: 301-314-9675')
+                # Update the last message sent time
+                last_message_sent_time = time.time()
+            # Add other command cases here...
+        else:
+            # If the cooldown period hasn't elapsed, do not send any messages
+            log("Cooldown period hasn't elapsed yet. Skipping message sending.")
     
     return "ok", 200
 
