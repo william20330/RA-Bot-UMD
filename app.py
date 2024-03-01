498faa74fb5c84c8ca5f54228035d5b262be96d0
William Phoone <william20330@gmail.com>
2024-03-01 12:54:05 -0500

    test

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
 import os
 import sys
 from urllib.parse import urlencode
-from urllib.request import urlopen
-from flask import Flask, Request, request
+from urllib.request import Request, urlopen
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def webhook():
-  data = request.get_json()
-  log('Recieved {}'.format(data))
+    data = request.get_json()
+    log('Received {}'.format(data))
 
-  # We don't want to reply to ourselves!
-  if data['name'] != 'ra bot':
     # Check if the message contains "ra bot"
-    if 'ra bot' in data['text'].lower():
+    if 'ra bot' in data['text'].lower() and data['name'] != 'ra bot':
         send_message('hi')
 
-  return "ok", 200
+    return "ok", 200
 
 def send_message(msg):
-  url  = 'https://api.groupme.com/v3/bots/post'
+    url = 'https://api.groupme.com/v3/bots/post'
 
-  data = {
-          'bot_id' : os.getenv('GROUPME_BOT_ID'),
-          'text'   : msg,
-         }
-  request = Request(url, urlencode(data).encode())
-  json = urlopen(request).read().decode()
-  
+    data = {
+        'bot_id': os.getenv('d333dc538150f9a45a0072029b'),
+        'text': msg,
+    }
+    request = Request(url, urlencode(data).encode())
+    response = urlopen(request)
+    log('Message sent: {}'.format(response.read().decode()))
+
 def log(msg):
-  print(str(msg))
-  sys.stdout.flush()
+    print(str(msg))
+    sys.stdout.flush()
+
+if __name__ == "__main__":
+    app.run()
