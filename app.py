381dd5b0c17f210e302460a033716f9336844a8e
William Phoone <william20330@gmail.com>
2024-03-01 18:33:06 -0500

    trying

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def start_scheduler():
 
 @app.route('/', methods=['POST'])
 def webhook():
-    global bot_active  # Accessing the global bot_active variable
+    global bot_active, last_message_sent_time  # Accessing global variables
 
     data = request.get_json()
     log('Received {}'.format(data))
@@ -51,24 +51,24 @@ def webhook():
         if time.time() - last_message_sent_time >= cooldown_period:
             if '/menu' in data['text'].lower():
                 send_message('Here are the options (press number associated with choice): \
-                        \n 1 - Phone Number for 4Work (issues regarding facilites, cleanliness, etc) \
+                        \n 1 - Phone Number for 4Work (issues regarding facilities, cleanliness, etc) \
                         \n 2 - Phone Number for the Cumberland Front Desk (contact RA on Duty, lockouts, etc) \
                         \n 3 - Hours of Operation For Dining Halls \
                         \n 4 - Important Links (ResLife, 4Work, etc)\
-                        \n 5 - Important Dates (closues, breaks, finals, etc)\
+                        \n 5 - Important Dates (closures, breaks, finals, etc)\
                         \n 6 - UMD Sports Schedule/Scores')
             elif '1' in data['text'] and bot_active:
                 send_message('Phone Number for 4Work: 301-314-9675')
-                last_message_sent_time = time.time()
+                last_message_sent_time = time.time()  # Update last message sent time
             elif '2' in data['text'] and bot_active:
                 send_message('Phone Number for the Cumberland Front Desk: 301-314-2862')
-                last_message_sent_time = time.time()
+                last_message_sent_time = time.time()  # Update last message sent time
             elif '3' in data['text'] and bot_active:
                 send_message('Dining Hall Hours: \
                         \n Yahentamitsi: Monday - Friday: 7:00am - 9:00pm | Saturday - Sunday: 10:00am - 9:00pm\
                         \n 251 North: Monday - Thursday: 8:00am - 10:00pm | Friday - Sunday: 8:00am - 7:00pm\
                         \n South Campus: Monday - Friday: 7:00am - 9:00pm | Saturday - Sunday: 10:00am - 9:00pm')
-                last_message_sent_time = time.time()
+                last_message_sent_time = time.time()  # Update last message sent time
             elif '4' in data['text'] and bot_active:
                 send_message('Important Links: \
                         \n ResLife: https://reslife.umd.edu/ \
@@ -76,18 +76,18 @@ def webhook():
                         \n UMD: https://www.umd.edu/\
                         \n Dining Services: https://dining.umd.edu/\
                         \n StarRez: https://www.starrez.umd.edu/')
-                last_message_sent_time = time.time()
+                last_message_sent_time = time.time()  # Update last message sent time
             elif '5' in data['text'] and bot_active:
                 send_message('Important Dates: \
                         \n Spring Break: March 17-24\
                         \n Last Day of Classes: May 9\
                         \n Reading Day: May 10\
                         \n Finals: May 11-17')
-                last_message_sent_time = time.time()
+                last_message_sent_time = time.time()  # Update last message sent time
             elif '/exit' in data['text'].lower() and bot_active:
                 send_message('Goodbye!')
                 bot_active = False  # Deactivate the bot
-                last_message_sent_time = time.time()
+                last_message_sent_time = time.time()  # Update last message sent time
     
     return "ok", 200
 
