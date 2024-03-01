2c916a4a2f1aef98277a53537b236836bfe1794e
William Phoone <william20330@gmail.com>
2024-03-01 13:33:30 -0500

    keep trying

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import os
 import sys
-from urllib.request import urlopen
 import requests
-from flask import Flask, Request, request
-import json
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -13,24 +11,21 @@ def webhook():
     log('Received {}'.format(data))
 
     # Check if the message contains "ra bot"
-    if '/ra' in data['text'].lower() and data['name'] != 'RA Bot':
+    if 'ra bot' in data['text'].lower() and data['name'] != 'RA Bot':
         send_message('hi')
 
     return "ok", 200
 
 def send_message(msg):
     url = 'https://api.groupme.com/v3/bots/post'
-
     data = {
         'bot_id': os.getenv('GROUPME_BOT_ID'),
         'text': msg,
     }
-    data = json.dumps(data).encode('utf-8')  # Encode the data to bytes
-    request = Request(url, data)  # Create the request object with data
-    request.add_header('Content-Type', 'application/json')  # Correct way to add headers
-
-    response = urlopen(request)
-    log('Message sent: {}'.format(response.read().decode()))
+    headers = {'Content-Type': 'application/json'}
+    
+    response = requests.post(url, json=data, headers=headers)
+    log('Message sent: {}'.format(response.text))
 
 def log(msg):
     print(str(msg))
